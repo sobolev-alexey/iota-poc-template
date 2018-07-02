a0477c58329f6ccd3bd851727011d13694842e6a
Alexey Sobolev <sobolev@mail.de>
2018-07-02 17:27:53 +0200

    Updated Firebase config

--- firebase_functions/functions/index.js
+++ firebase_functions/functions/index.js
@@ -2,7 +2,6 @@ const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const serviceAccount = require('./service_account.json');
 const login = require('./login');
-const list = require('./list');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -10,4 +9,3 @@ admin.initializeApp({
 });
 
 exports.login = functions.https.onRequest(login);
-exports.list = functions.https.onRequest(list);
